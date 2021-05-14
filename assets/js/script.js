bae2c926ad8498b027901d685c582e1ca74f75d0
Jaime Lizama R <jlizama@poptimize.cl>
2021-05-13 22:11:10 -0400

    Código Identado
    Código identado con plugins
    (Probando las extensiones para mejores prácticas)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,40 @@
-$(document).ready(function(){
-    
-    //Smooth Scroll
-    $("a").click(function(event){
-        if (this.hash !== "") {
-            event.preventDefault();
+$(document).ready(function () {
+  //Smooth Scroll
+  $("a").click(function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
 
-            var gato = this.hash;
+      var gato = this.hash;
 
-            $("html, body").animate({
-                scrollTop: $(gato).offset().top
-            }, 800, function(){
-                window.location.hash = gato;
-            })
+      $("html, body").animate(
+        {
+          scrollTop: $(gato).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = gato;
         }
-    });
+      );
+    }
+  });
 
-    //Cambio de color del navbar al hacer scroll
-    $(window).scroll(function(){
-        $('nav').toggleClass('scrolled', $(this).scrollTop()>50);
-    });
-    
+  //Cambio de color del navbar al hacer scroll
+  $(window).scroll(function () {
+    $("nav").toggleClass("scrolled", $(this).scrollTop() > 50);
+  });
 
-    //Tooltip Sección Quienes Somos
-    $('[data-toggle="tooltip"]').tooltip()
+  //Tooltip Sección Quienes Somos
+  $('[data-toggle="tooltip"]').tooltip();
 
-    
-    //Hover Sección Destacados (ingreso del cursor)
-    $('.col-md-3').hover(function(){
-        $(this).animate({marginTop: "-=1%",
-        },200);
+  //Hover Sección Destacados (ingreso del cursor)
+  $(".col-md-3").hover(
+    function () {
+      $(this).animate({ marginTop: "-=1%" }, 200);
     },
 
-    //Hover Sección Destacados (salida del cursor)    
-    function(){
-        $(this).animate({marginTop: "0%",
-        },200);
-    });
-
+    //Hover Sección Destacados (salida del cursor)
+    function () {
+      $(this).animate({ marginTop: "0%" }, 200);
+    }
+  );
 });
